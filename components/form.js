3ebf387378421f68b3b3538e47d886d6068529f2
Santiago Persico <spersico@railz.ai>
2023-01-25 14:42:11 +0000

    prepare form for more fields

--- components/form.js
+++ components/form.js
@@ -2,6 +2,7 @@ import { Calendar } from 'primereact/calendar';
 
 import useInflationData from '../logic/useInflation';
 import CountrySelect from './CountrySelect';
+import styles from './Form.module.css';
 
 export default function Form({ countries, router }) {
   const { setCountry, setDate, selectedCountry, selectedDate } =
@@ -10,31 +11,30 @@ export default function Form({ countries, router }) {
   const [year, month] = selectedCountry ? selectedCountry.first.split('-') : [new Date().getFullYear(), '01'];
 
   return (
-    <div className='flex justify-content-center'>
-      <div className='card'>
-        <h5 className='text-center'>Select A Country</h5>
-        <form onSubmit={console.log} className='p-fluid'>
+    <form onSubmit={console.log} className='p-fluid'>
+      <div className={styles.formWrapper}>
+        <div className='card'>
+          <label htmlFor='countryPicker'>Select a Country</label>
           <CountrySelect
             value={selectedCountry}
             countries={countries}
-            onChange={(country) => setCountry(country.code)}
+            onChange={(country) => setCountry(country?.code)}
           />
-
-          <div className='field col-12'>
-            <label htmlFor='monthpicker'>Month Picker</label>
-            <Calendar
-              id='monthpicker'
-              value={selectedDate}
-              disabled={!selectedCountry}
-              onChange={(e) => setDate(e.value)}
-              view='month'
-              minDate={new Date(`${year}-${month}-15`)}
-              maxDate={new Date()}
-              dateFormat='mm/yy'
-            />
-          </div>
-        </form>
-      </div>
-    </div>
+        </div>
+        <div className='card'>
+          <label htmlFor='monthPicker'>Select a Date</label>
+          <Calendar
+            inputId='monthPicker'
+            value={selectedDate}
+            disabled={!selectedCountry}
+            onChange={(e) => setDate(e.value)}
+            view='month'
+            minDate={new Date(`${year}-${month}-15`)}
+            maxDate={new Date()}
+            dateFormat='mm/yy'
+          />
+        </div>
+      </div >
+    </form>
   );
 }
