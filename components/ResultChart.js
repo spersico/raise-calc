f5372b126a16f0e00f7ff4ca550bc7c2b1e1227f
SP <spersico@railz.ai>
2023-03-26 18:54:55 -0300

    add ugly but working graph

---
+++ components/ResultChart.js
@@ -0,0 +1,130 @@
+import React, { useRef, useEffect, useState } from 'react';
+import {
+    Chart as ChartJS,
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip,
+    Filler,
+} from 'chart.js';
+import { Chart } from 'react-chartjs-2';
+
+ChartJS.register(
+    LinearScale,
+    CategoryScale,
+    BarElement,
+    PointElement,
+    LineElement,
+    Legend,
+    Tooltip,
+    Filler
+);
+
+
+const options = {
+    plugins: {
+        tooltip: { position: 'nearest' },
+        filler: {
+            propagate: false,
+        },
+        title: {
+            display: true,
+            text: (ctx) => 'drawTime: ' + ctx.chart.options.plugins.filler.drawTime
+        }
+    },
+
+    scales: {
+        x: {
+            border: {
+                display: false
+            },
+            grid: {
+                display: true,
+                drawOnChartArea: true,
+                drawTicks: true,
+                color: 'rgba(255, 255, 255, 0.25)',
+            }
+        },
+        y: {
+            border: {
+                display: true
+            },
+            grid: {
+                display: false
+            },
+        }
+    },
+    animations: {
+        y: {
+            easing: 'easeInOutElastic',
+            from: (ctx) => {
+                if (ctx.type === 'data') {
+                    if (ctx.mode === 'default' && !ctx.dropped) {
+                        ctx.dropped = true;
+                        return 0;
+                    }
+                }
+            }
+        }
+    },
+    interaction: {
+        intersect: false,
+        mode: 'index',
+    },
+};
+const buyingPowerAtAPoint = (initialSalary) => (point) => initialSalary / (1 + (point.acumulatedInflation / 100));
+
+export const generateDataset = (dataPoints, initialSalary) => {
+    const buyingPointRelativeToSalary = buyingPowerAtAPoint(initialSalary);
+    return {
+        labels: dataPoints.map((dataPoint) => dataPoint.date),
+        datasets: [
+            {
+                label: 'Your Salary',
+                data: dataPoints.map(() => initialSalary),
+                borderColor: 'rgba(0, 255, 91, 1)',
+                animations: {
+                    y: {
+                        duration: 2000,
+                    }
+                },
+                radius: 0,
+                borderWidth: 3,
+                backgroundColor: 'rgba(255, 0, 0, 0.25)',
+                fill: 1,
+                tension: 0.5
+            },
+            {
+                label: 'Your Buying Power',
+                borderColor: 'rgba(238, 255, 0, 1)',
+                backgroundColor: 'rgba(0, 255, 91, 0.25)',
+                animations: {
+                    y: {
+                        duration: 2000,
+                        delay: 1000
+                    }
+                },
+                fill: true,
+                radius: 1,
+                data: [initialSalary, ...dataPoints.map(buyingPointRelativeToSalary)],
+                beginAtZero: true
+
+            },
+        ],
+    };
+};
+
+
+
+
+export function BuyingPowerGraph({ data, initialValue = 1000 }) {
+    const [chartData, setChartData] = useState(generateDataset([]));
+    useEffect(() => {
+        data.length && setChartData(generateDataset(data, initialValue));
+    }, [data, initialValue]);
+
+    return <Chart type='line' data={chartData} options={options} />;
+}
--- components/ResultSummary.js
+++ components/ResultSummary.js
@@ -1,11 +1,21 @@
+import { Accordion, AccordionTab } from 'primereact/accordion';
+
+import { BuyingPowerGraph } from './ResultChart';
 
 
 export default function ResultSummary({ country }) {
     return (
         <>
-            RESULTS
-            <br />
-            <pre>{JSON.stringify(country, null, 2)}</pre>
+            <BuyingPowerGraph data={country.periods} />
+
+            <Accordion activeIndex={0}>
+                <AccordionTab header="Raw Data">
+                    <pre>{JSON.stringify(country, null, 2)}</pre>
+                </AccordionTab>
+            </Accordion>
+
+
+
         </>
     );
 }
