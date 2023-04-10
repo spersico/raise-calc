01d024eacb4a686af4e0f40d04e1aa7570e4f535
SP <spersico@railz.ai>
2023-04-09 21:14:56 -0300

    add currencyCode to slimCountryList

--- countryData/dataUnifiers/slimCountryList.js
+++ countryData/dataUnifiers/slimCountryList.js
@@ -1,7 +1,7 @@
 import { writeFile } from 'fs/promises';
 import countries from 'world-countries';
 import logger from '../../utils/logger.js';
-import { DATA_FOLDER } from '../constants.js';
+import { DATA_FOLDER, CURRENCY_INFO_FALLBACK } from '../constants.js';
 
 
 const allCountries = countries
@@ -16,18 +16,24 @@ const allCountries = countries
       name: { common, native },
       languages,
       latlng,
-    }) => ({
-      codes: { cca2, cioc, cca3 },
-      names: [
+    }) => {
+
+      const names = [
         common,
         ...Object.keys(native)
           .filter((key) => key !== 'eng' && native[key].common !== common)
-          .map((key) => native[key].common),
-      ],
-      geo: { region, subregion, latlng },
-      languages,
-      currencies,
-    })
+          .map((key) => native[key].common)
+      ];
+      const currencyCodes = Object.entries(currencies);
+
+      return {
+        codes: { cca2, cioc, cca3 },
+        names,
+        geo: { region, subregion, latlng },
+        languages,
+        currencyCode: currencyCodes.length === 0 ? CURRENCY_INFO_FALLBACK[0] : currencyCodes[0][0],
+      };
+    }
   )
   .sort((a, b) => a.names[0] - b.names[0]);
 
@@ -38,11 +44,13 @@ const slimCountryList = (allData) =>
       const {
         codes: { cca2: code },
         names,
+        currencyCode,
       } = country;
 
       return {
         code,
         names,
+        currencyCode,
         first: allData[code].periods[0][0],
       };
     });
